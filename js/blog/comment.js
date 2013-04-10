068285fc6696619ed60b04e10e6c5f974818eafc
Gordon Lesti <gordon@lesti-webentwicklung.de>
2013-04-10 16:26:04 +0200

    moving from reply-form

--- js/blog/comment.js
+++ js/blog/comment.js
@@ -5,3 +5,25 @@
  * Time: 14:42
  * To change this template use File | Settings | File Templates.
  */
+
+function moveReplyForm(comment)
+{
+    // move reply-form
+    $('comment-' + comment).insert({'bottom': $('post-response')});
+    // add "cancel reply"
+    if($('parent_id').getValue() == "") {
+        $('comment-form').insert({'top': '<a id="cancel-reply" onclick="cancelReply();">' + Translator.translate('Cancel Reply') + '</a>' });
+    }
+    // set parent_id
+    $('parent_id').setValue(comment);
+}
+
+function cancelReply()
+{
+    // move reply-form
+    $('post-comments').insert({'bottom': $('post-response')});
+    // remove "cancel reply"
+    $('cancel-reply').remove();
+    // set parent_id
+    $('parent_id').setValue("");
+}
