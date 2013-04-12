ce270719e951a0349d2200d9fa3b53ba67761d98
Gordon Lesti <info@gordonlesti.com>
2013-04-12 16:56:32 +0200

    add commentcount

--- app/code/community/Lesti/Blog/Model/Resource/Post/Comment.php
+++ app/code/community/Lesti/Blog/Model/Resource/Post/Comment.php
@@ -32,7 +32,34 @@ class Lesti_Blog_Model_Resource_Post_Comment extends Mage_Core_Model_Resource_Db
             $object->setCreationTime(Mage::getSingleton('core/date')->gmtDate());
         }
 
+        $content = $object->getContent();
+        $content = Mage::helper('blog/post_comment')->purifyHtml($content);
+        $object->setContent();
+
         return parent::_beforeSave($object);
+    }
+
+    protected function _refreshPostCommentCount(Mage_Core_Model_Abstract $object)
+    {
+        $post = Mage::getModel('blog/post')->load($object->getPostId());
+        if($post->getId()) {
+            $commentCollection = Mage::getModel('blog/post_comment')->getCollection()
+                ->addFieldToFilter('status', Lesti_Blog_Model_Post_Comment::STATUS_ENABLED)
+                ->addFieldToFilter('post_id', $post->getId());
+            $post->setCommentCount($commentCollection->count())->save();
+        }
+    }
+
+    protected function _afterSave(Mage_Core_Model_Abstract $object)
+    {
+        $this->_refreshPostCommentCount($object);
+        return parent::_afterSave($object);
+    }
+
+    protected function _afterDelete(Mage_Core_Model_Abstract $object)
+    {
+        $this->_refreshPostCommentCount($object);
+        return parent::_afterDelete($object);
     }
 
 }
