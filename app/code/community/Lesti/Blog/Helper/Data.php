c28a3678a25528b5e73e0e363b2b8f74f0326b27
Gordon Lesti <info@gordonlesti.com>
2013-04-14 23:51:41 +0200

    replace date with Zend_Date

--- app/code/community/Lesti/Blog/Helper/Data.php
+++ app/code/community/Lesti/Blog/Helper/Data.php
@@ -8,6 +8,8 @@
  */
 class Lesti_Blog_Helper_data extends Mage_Core_Helper_Abstract
 {
+
+    const MYSQL_DATE_FORMAT = 'yyyy-MM-dd HH:mm:ss';
     /**
      * Retrieve blog url
      *
@@ -16,5 +18,11 @@ class Lesti_Blog_Helper_data extends Mage_Core_Helper_Abstract
     public function getBlogUrl()
     {
         return $this->_getUrl(Mage::getStoreConfig(Lesti_Blog_Model_Post::XML_PATH_BLOG_GENERAL_ROUTER));
+    }
+
+    public function formatDate($date, $format)
+    {
+        $date = new Zend_Date($date, self::MYSQL_DATE_FORMAT);
+        return $date->toString($format);
     }
 }
