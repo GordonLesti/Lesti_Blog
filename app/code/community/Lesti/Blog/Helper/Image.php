e93ff7a470c4add528c97234af25f5c8bc5d6445
Brian Matthews <brian@marketstrong.com>
2015-10-15 10:59:31 -0500

    added image helper

---
+++ app/code/community/Lesti/Blog/Helper/Image.php
@@ -0,0 +1,78 @@
+<?php
+
+/**
+ * Class Lesti_Blog_Helper_Image
+ *
+ *  For resizing post images
+ *
+ *  TODO: hook into cache system to remove files when cleared
+ */
+class Lesti_Blog_Helper_Image extends Mage_Core_Helper_Abstract
+{
+
+    const IMAGE_DIR = 'blog';
+
+
+    /**
+     * Resize image and return associative array of data
+     * 
+     * @param $post
+     * @param $width
+     * @param $height
+     * @return array|void 
+     */
+    public function resize($post, $width, $height = null) {
+
+        $imgData = array();
+
+        if (!$post->getMainImage()) {
+            return ;
+        }
+        // filesystem path of image
+        $originalImgPath = $post->getMainImageFilePath();
+
+        $imgFile =  $width . '-' . $height . '-' . str_replace(self::IMAGE_DIR . DS, '', $post->getMainImage());
+
+        $resizedFile = Mage::getBaseDir('media') . DS . self::IMAGE_DIR . DS . "resized" . DS  . $imgFile;
+
+        if (!file_exists($resizedFile) && file_exists($originalImgPath)) {
+            try {
+                $imageObj = new Varien_Image($originalImgPath);
+                $imageObj->keepFrame(false);
+                $imageObj->keepAspectRatio(true);
+                $imageObj->constrainOnly(true);
+                $imageObj->resize($width, $height);
+                $imageObj->save($resizedFile);
+                $imgData['width'] = $imageObj->getOriginalWidth();
+                $imgData['height'] = $imageObj->getOriginalHeight();
+            } catch (Exception $e) {
+                Mage::logException($e);
+                $imgData['url'] = $post->getMainImageUrl();
+            }
+        } else {
+            $imageObj = new Varien_Image($resizedFile);
+            $imgData['width'] = $imageObj->getOriginalWidth();
+            $imgData['height'] = $imageObj->getOriginalHeight();
+        }
+
+        $imgData['url'] = Mage::getUrl('media') . self::IMAGE_DIR . DS . "resized" . DS . $imgFile;
+        return $imgData;
+    }
+
+    public function getPostImage($post, $width, $height = null, $attributes = array()) {
+
+        $imgData = $this->resize($post, $width, $height);
+        $attributes['src'] = $imgData['url'];
+        $attributes['width'] = $imgData['width'];
+        $attributes['height'] = $imgData['height'];
+        $attributes['alt'] = Mage::helper('core')->quoteEscape($post->getTitle());
+        
+        $atts = '';
+        foreach($attributes as $key => $value){
+            $atts .= $key .'="' .$value .'" ';
+        }
+        $html = '<img  '.$atts.' />';
+        
+        return $html;
+    }
+}
