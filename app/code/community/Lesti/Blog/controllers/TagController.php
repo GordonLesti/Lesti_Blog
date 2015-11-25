5fda4762c63300e81eafb1b7caa1c499125e6932
Cameron McDonald <cameron@marketstrong.com>
2015-11-25 15:42:37 -0600

    added tag view

--- app/code/community/Lesti/Blog/controllers/TagController.php
+++ app/code/community/Lesti/Blog/controllers/TagController.php
@@ -24,13 +24,10 @@ class Lesti_Blog_TagController extends Mage_Core_Controller_Front_Action
         if ($root = $this->getLayout()->getBlock('root')) {
             $root->addBodyClass('blog');
         }
-        if($view = $this->getLayout()->getBlock('blog.category.view')) {
-            $view->setType(Lesti_Blog_Block_Category_View::OBJECT_TYPE_TAG);
-            $view->setObject($tag);
-        }
-        $title = Mage::getStoreConfig(Lesti_Blog_Block_Category_View::XML_PATH_BLOG_TITLE) . ' - ' . $tag->getTitle();
+        $title = Mage::getStoreConfig(Lesti_Blog_Block_View::XML_PATH_BLOG_TITLE) . ' - ' . $tag->getTitle();
         $this->getLayout()->getBlock('head')
             ->setTitle($title);
+        $this->getLayout()->getBlock('content')->getChild('blog.posts.view')->setTag($tag);
         $this->renderLayout();
     }
 }
