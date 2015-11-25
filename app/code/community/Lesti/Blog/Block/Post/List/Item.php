364ba0df609f777446e6fb249472b4cf405d5ebf
Cameron McDonald <cameron@marketstrong.com>
2015-11-25 11:47:03 -0600

    added post list block and removed 'category' block/template

---
+++ app/code/community/Lesti/Blog/Block/Post/List/Item.php
@@ -0,0 +1,28 @@
+<?php
+
+class Lesti_Blog_Block_Post_List_Item extends Mage_Core_Block_Template
+{
+    /**
+     * @param int $categoryId
+     * @return Lesti_Blog_Model_Category
+     */
+    public function getCategory($categoryId) {
+        $categories = $this->getCategoriesCache();
+        if (! empty($categories[$categoryId])) {
+            return $categories[$categoryId];
+        }
+        return null;
+    }
+    
+    /**
+     * @param int $tagId
+     * @return Lesti_Blog_Model_Tag
+     */
+    public function getTag($tagId) {
+        $categories = $this->getTagsCache();
+        if (! empty($categories[$tagId])) {
+            return $categories[$tagId];
+        }
+        return null;
+    }
+}
---
+++ app/code/community/Lesti/Blog/Block/View.php
@@ -0,0 +1,6 @@
+<?php
+
+class Lesti_Blog_Block_View extends Mage_Core_Block_Template
+{
+    
+}
